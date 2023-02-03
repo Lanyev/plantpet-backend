146bee5b356d183af83d66e63c88b6301441342f
Alan Yeverino <lanyev@gmail.com>
2023-02-02 21:49:30 -0700

    Added models categories

--- config.js
+++ config.js
@@ -1,52 +1,51 @@
 //? Aqui  definiremos todas las Enviroment Variables
 //? Aqui  definiremos todas las Enviroment Variables
-require('dotenv').config()
+require("dotenv").config();
 
 const configs = {
-    api:{
-        nodeEnv: process.env.NODE_ENV,
-        port: process.env.PORT,
-        host: process.env.HOST,
+  api: {
+    nodeEnv: process.env.NODE_ENV,
+    port: process.env.PORT,
+    host: process.env.HOST,
+  },
+  db: {
+    development: {
+      dialect: "postgres",
+      host: "",
+      username: "",
+      password: "",
+      database: "",
+      define: {
+        timestamps: true, //? Va a utilizar por defecto el created_at y updated_at
+        underscored: true, //? Convierte camelCase en snake_case en todos los nombres y atributos de mi modelo
+        underscoredAll: true, //?
+      },
     },
-    db:{
-        development:{
-            dialect: 'postgres',
-            host: '',
-            username: '',
-            password: '',
-            database: '',
-            define:{
-                timestamps:true, //? Va a utilizar por defecto el created_at y updated_at
-                underscored: true, //? Convierte camelCase en snake_case en todos los nombres y atributos de mi modelo
-                underscoredAll: true //? 
-            }
-        },
-        test:
-        {
-            dialect: 'postgres',
-            host: '',
-            username: '',
-            password: '',
-            database: '',
-            define:{
-                timestamps:true, //? Va a utilizar por defecto el created_at y updated_at
-                underscored: true, //? Convierte camelCase en snake_case en todos los nombres y atributos de mi modelo
-                underscoredAll: true //? 
-            }
-        },
-        production:{
-            dialect: 'postgres',
-            host: '',
-            username: '',
-            password: '',
-            database: '',
-            define:{
-                timestamps:true, //? Va a utilizar por defecto el created_at y updated_at
-                underscored: true, //? Convierte camelCase en snake_case en todos los nombres y atributos de mi modelo
-                underscoredAll: true //? 
-            }
-        }
-    }
-}
+    test: {
+      dialect: "postgres",
+      host: "",
+      username: "",
+      password: "",
+      database: "",
+      define: {
+        timestamps: true, //? Va a utilizar por defecto el created_at y updated_at
+        underscored: true, //? Convierte camelCase en snake_case en todos los nombres y atributos de mi modelo
+        underscoredAll: true, //?
+      },
+    },
+    production: {
+      dialect: "postgres",
+      host: "",
+      username: "",
+      password: "",
+      database: "",
+      define: {
+        timestamps: true, //? Va a utilizar por defecto el created_at y updated_at
+        underscored: true, //? Convierte camelCase en snake_case en todos los nombres y atributos de mi modelo
+        underscoredAll: true, //?
+      },
+    },
+  },
+};
 
-module.exports = configs
+module.exports = configs;
